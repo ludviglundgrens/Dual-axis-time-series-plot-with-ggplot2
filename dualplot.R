9d1a6e98ca306fb3987d2f4a98339602222444b4
ludviglundgrens <48127020+ludviglundgrens@users.noreply.github.com>
2020-11-23 23:59:47 +0100

    Check that date is in right format

--- dualplot.R
+++ dualplot.R
@@ -2,6 +2,9 @@ dplot <- function(df1, date1, df2, date2) {
   k <- df1[!is.na(df1)]
   l <- df2[!is.na(df2)]
   
+  date1 <- as.Date(date1) 
+  date2 <- as.Date(date2)
+  
   fact <- (max(k)-min(k))/(max(l)-min(l))
   
   mean_1 <- mean(k)
@@ -9,8 +12,8 @@ dplot <- function(df1, date1, df2, date2) {
   diff <- mean_1-mean_2
   
   ggplot2::ggplot()+
-    geom_line(aes(x = date, y = ((df1-mean_1)/fact)+mean_1-diff))+
-    geom_line(aes(x = date, y = df2), col = "red")+
+    geom_line(aes(x = date1, y = ((df1-mean_1)/fact)+mean_1-diff))+
+    geom_line(aes(x = date2, y = df2), col = "red")+
     scale_y_continuous(sec.axis = sec_axis(~(.+diff-mean_1)*fact+mean_1))+
     labs(x = "xlabs", y = "ylabs")
 }
