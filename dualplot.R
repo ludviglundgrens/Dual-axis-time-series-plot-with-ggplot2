6a6f473b58fde3884fe775db5298a4e9bf5f8005
ludviglundgrens <48127020+ludviglundgrens@users.noreply.github.com>
2020-12-05 01:00:57 +0100

    minor updates

--- dualplot.R
+++ dualplot.R
@@ -1,5 +1,5 @@
-dplot <- function(df1, date1, df2, date2, name1 = "Series 1", name2 = "Series 2", xlab = "xlab", ylab = "ylab", ylab2 = "ylab", breaks = "2 years") {
-  cols <- c("#004DBA", "#6BA3FF")
+dplot <- function(df1, date1, df2, date2, name1 = "Series 1", name2 = "Series 2", xlab = "xlab", ylab = "ylab", ylab2 = "ylab", breaks = "2 years", title = "Title", subtitle="", caption = "") {
+  cols <- c("#636a75", "#6fa6e8")
   k <- df1[!is.na(df1)]
   l <- df2[!is.na(df2)]
   
@@ -16,7 +16,7 @@ dplot <- function(df1, date1, df2, date2, name1 = "Series 1", name2 = "Series 2"
     geom_line(aes(x = date1, y = ((df1-mean_1)/fact)+mean_1-diff, color = name1))+
     geom_line(aes(x = date2, y = df2, color = name2))+
     scale_y_continuous(sec.axis = sec_axis(~(.+diff-mean_1)*fact+mean_1, name = "hej"))+
-    labs(x = xlab, y = ylab)+
+    labs(x = xlab, y = ylab, title = title, subtitle = subtitle, caption = caption)+
     theme_light()+
     scale_colour_manual(values=cols)+
     theme(legend.position="bottom", legend.box = "horizontal")+
