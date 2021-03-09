f93ad15decaf56255a81e38c70b750eb3e8e1d0a
ludviglundgrens <48127020+ludviglundgrens@users.noreply.github.com>
2021-03-09 16:25:12 +0100

    Update dualplot.R
    correcting error in label

--- dualplot.R
+++ dualplot.R
@@ -14,8 +14,8 @@ dplot <- function(df1, date1, df2, date2, name1 = "Series 1", name2 = "Series 2"
   
   return(
     ggplot2::ggplot()+
-      geom_line(aes(x = date1, y = ((df1-mean_1)/fact)+mean_1-diff, color = name1))+
-      geom_line(aes(x = date2, y = df2, color = name2))+
+      geom_line(aes(x = date1, y = ((df1-mean_1)/fact)+mean_1-diff, color = name2))+
+      geom_line(aes(x = date2, y = df2, color = name1))+
       scale_y_continuous(sec.axis = sec_axis(~(.+diff-mean_1)*fact+mean_1, name = ylab2))+
       labs(x = xlab, y = ylab, title = title, subtitle = subtitle, caption = caption)+
       theme_light()+
@@ -25,5 +25,5 @@ dplot <- function(df1, date1, df2, date2, name1 = "Series 1", name2 = "Series 2"
       theme(panel.grid.major.x = element_blank())+
       theme(panel.grid.minor.x = element_blank())+
       scale_x_date(breaks = breaks, date_labels = "%Y")
-    )
+  )
 }
