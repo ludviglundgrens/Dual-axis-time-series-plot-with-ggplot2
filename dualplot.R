298b9fbfb39081f29a2d162f0198f7dade1d8ce0
ludviglundgrens <48127020+ludviglundgrens@users.noreply.github.com>
2021-03-09 16:12:52 +0100

    Update dualplot.R

--- dualplot.R
+++ dualplot.R
@@ -12,16 +12,18 @@ dplot <- function(df1, date1, df2, date2, name1 = "Series 1", name2 = "Series 2"
   mean_2 <- mean(l)
   diff <- mean_1-mean_2
   
-  ggplot2::ggplot()+
-    geom_line(aes(x = date1, y = ((df1-mean_1)/fact)+mean_1-diff, color = name1))+
-    geom_line(aes(x = date2, y = df2, color = name2))+
-    scale_y_continuous(sec.axis = sec_axis(~(.+diff-mean_1)*fact+mean_1, name = ylab2))+
-    labs(x = xlab, y = ylab, title = title, subtitle = subtitle, caption = caption)+
-    theme_light()+
-    scale_colour_manual(values=cols)+
-    theme(legend.position="bottom", legend.box = "horizontal")+
-    theme(legend.title=element_blank())+
-    theme(panel.grid.major.x = element_blank())+
-    theme(panel.grid.minor.x = element_blank())+
-    scale_x_date(breaks = breaks, date_labels = "%Y")
+  return(
+    ggplot2::ggplot()+
+      geom_line(aes(x = date1, y = ((df1-mean_1)/fact)+mean_1-diff, color = name1))+
+      geom_line(aes(x = date2, y = df2, color = name2))+
+      scale_y_continuous(sec.axis = sec_axis(~(.+diff-mean_1)*fact+mean_1, name = ylab2))+
+      labs(x = xlab, y = ylab, title = title, subtitle = subtitle, caption = caption)+
+      theme_light()+
+      scale_colour_manual(values=cols)+
+      theme(legend.position="bottom", legend.box = "horizontal")+
+      theme(legend.title=element_blank())+
+      theme(panel.grid.major.x = element_blank())+
+      theme(panel.grid.minor.x = element_blank())+
+      scale_x_date(breaks = breaks, date_labels = "%Y")
+    )
 }
